1fd5f58fabccb9572c378bd0b130c8be66502c16
sid0167 <144150869+sid0167@users.noreply.github.com>
2025-04-06 14:17:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 
 app.use('/api/health', healthRoutes);
 app.use('/api/auth', authRoutes);
+app.get('/', (req, res) => {
+  res.send('IoT Health API is running ✅');
+});
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
