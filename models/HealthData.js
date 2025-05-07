7e4099115c2f6e3231cb7ac6b4c868a47cfa6b61
sid0167 <siddang2005@gmail.com>
2025-05-07 08:10:04 +0530

    Update HealthData.js

--- models/HealthData.js
+++ models/HealthData.js
@@ -5,7 +5,7 @@ const healthDataSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // ✅ rename to 'user'
   temperature: Number, // ✅ ADD this field if you're using it in your dashboard
   humidity: Number,
-  AIR: Number, // ✅ ADD this field if you're using it in your dashboard
+  air: Number, // ✅ ADD this field if you're using it in your dashboard
   timestamp: { type: Date, default: Date.now }
 });
 
