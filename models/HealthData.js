f6b77e48079d486bb1bc6f54131c84914de3b5f7
sid0167 <siddang2005@gmail.com>
2025-05-07 21:59:42 +0530

    spo2 change

--- models/HealthData.js
+++ models/HealthData.js
@@ -5,7 +5,7 @@ const healthDataSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // ✅ rename to 'user'
   heartRate: Number,
   temperature: Number,
-  bloodPressure: String, // ✅ ADD this field if you're using it in your dashboard
+  spo2: String, // ✅ ADD this field if you're using it in your dashboard
   timestamp: { type: Date, default: Date.now }
 });
 
